64ab54e7796f969f85b917898f6b481a71e027a2
mcpedrosa <mcpedrosa@stefanini.com>
2017-04-16 09:56:02 -0300

    Change in method names.

--- src/main/java/br/com/mk/util/FacesUtil.java
+++ src/main/java/br/com/mk/util/FacesUtil.java
@@ -1,29 +1,28 @@
 package br.com.mk.util;
 
-
 import br.com.mk.producers.FacesProducer;
 
 import javax.faces.application.FacesMessage;
 
 public class FacesUtil {
 
-    public static void sucesso(String mensagem){
-        FacesProducer.getFacesContext().addMessage(null, getMensagem(FacesMessage.SEVERITY_INFO, mensagem));
+    public static void sucess(String message){
+        FacesProducer.getFacesContext().addMessage(null, getMessage(FacesMessage.SEVERITY_INFO, message));
     }
 
-    public static void aviso(String mensagem){
-        FacesProducer.getFacesContext().addMessage(null, getMensagem(FacesMessage.SEVERITY_WARN, mensagem));
+    public static void warn(String message){
+        FacesProducer.getFacesContext().addMessage(null, getMessage(FacesMessage.SEVERITY_WARN, message));
     }
 
-    public static void error(String mensagem){
-        FacesProducer.getFacesContext().addMessage(null, getMensagem(FacesMessage.SEVERITY_ERROR, mensagem));
+    public static void error(String message){
+        FacesProducer.getFacesContext().addMessage(null, getMessage(FacesMessage.SEVERITY_ERROR, message));
     }
 
-    public static void fatal(String mensagem){
-        FacesProducer.getFacesContext().addMessage(null, getMensagem(FacesMessage.SEVERITY_FATAL, mensagem));
+    public static void fatal(String message){
+        FacesProducer.getFacesContext().addMessage(null, getMessage(FacesMessage.SEVERITY_FATAL, message));
     }
 
-    private static FacesMessage getMensagem(FacesMessage.Severity level, String mensagem){
+    private static FacesMessage getMessage(FacesMessage.Severity level, String mensagem){
         return new FacesMessage(level, mensagem, null);
     }
 }
