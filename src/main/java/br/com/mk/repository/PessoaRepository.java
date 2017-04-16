d25faad0a9ae3e22749f96efcf6bd56f058c7c8d
mcpedrosa <mcpedrosa@stefanini.com>
2017-04-16 10:47:39 -0300

    Add methods in interface.

--- src/main/java/br/com/mk/repository/PessoaRepository.java
+++ src/main/java/br/com/mk/repository/PessoaRepository.java
@@ -4,8 +4,21 @@ package br.com.mk.repository;
 import br.com.mk.entity.Pessoa;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 public interface PessoaRepository extends Serializable{
 
     Pessoa save(Pessoa pessoa);
+
+    Pessoa change(Pessoa pessoa);
+
+    Boolean delete(Pessoa pessoa);
+
+    void delete(Long id);
+
+    Pessoa find(Long id);
+
+    Pessoa find(Pessoa pessoa);
+
+    Collection<Pessoa> findAll();
 }
