fb7ebeb68f355fe8309ebb27d597784bc1f703cf
mcpedrosa <mcpedrosa@stefanini.com>
2017-04-16 10:47:51 -0300

    Add methods.

--- src/main/java/br/com/mk/repository/impl/PessoaRepositoryImpl.java
+++ src/main/java/br/com/mk/repository/impl/PessoaRepositoryImpl.java
@@ -6,6 +6,9 @@ import br.com.mk.repository.PessoaRepository;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.Collection;
 
 public class PessoaRepositoryImpl implements PessoaRepository {
 
@@ -16,5 +19,41 @@ public class PessoaRepositoryImpl implements PessoaRepository {
     public Pessoa save(Pessoa pessoa) {
         this.entityManager.persist(pessoa);
         return pessoa;
+    }
+
+    @Override
+    public Pessoa change(Pessoa pessoa) {
+        return this.entityManager.merge(pessoa);
+    }
+
+    @Override
+    public Boolean delete(Pessoa pessoa) {
+        this.entityManager.remove(pessoa);
+        return true;
+    }
+
+    @Override
+    public void delete(Long id) {
+        Pessoa pessoa = this.find(id);
+        this.entityManager.remove(pessoa);
+    }
+
+    @Override
+    public Pessoa find(Long id) {
+        return this.entityManager.find(Pessoa.class, id);
+    }
+
+    @Override
+    public Pessoa find(Pessoa pessoa) {
+        return this.entityManager.find(pessoa.getClass(), pessoa.getId());
+    }
+
+    @Override
+    public Collection<Pessoa> findAll() {
+        CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pessoa> criteria = builder.createQuery(Pessoa.class);
+        criteria.from(Pessoa.class);
+
+        return this.entityManager.createQuery(criteria).getResultList();
     }
 }
