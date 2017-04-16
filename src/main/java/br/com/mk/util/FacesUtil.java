6a7d11417bf3c9eb49814ef62851941b5bfcbde3
mcpedrosa <mcpedrosa@stefanini.com>
2017-04-16 11:39:31 -0300

    Change methods to message.

--- src/main/java/br/com/mk/util/FacesUtil.java
+++ src/main/java/br/com/mk/util/FacesUtil.java
@@ -7,22 +7,26 @@ import javax.faces.application.FacesMessage;
 public class FacesUtil {
 
     public static void sucess(String message){
-        FacesProducer.getFacesContext().addMessage(null, getMessage(FacesMessage.SEVERITY_INFO, message));
+        message(FacesMessage.SEVERITY_INFO, message);
     }
 
     public static void warn(String message){
-        FacesProducer.getFacesContext().addMessage(null, getMessage(FacesMessage.SEVERITY_WARN, message));
+        message(FacesMessage.SEVERITY_WARN, message);
     }
 
     public static void error(String message){
-        FacesProducer.getFacesContext().addMessage(null, getMessage(FacesMessage.SEVERITY_ERROR, message));
+        message(FacesMessage.SEVERITY_ERROR, message);
     }
 
     public static void fatal(String message){
-        FacesProducer.getFacesContext().addMessage(null, getMessage(FacesMessage.SEVERITY_FATAL, message));
+        message(FacesMessage.SEVERITY_FATAL, message);
     }
 
-    private static FacesMessage getMessage(FacesMessage.Severity level, String mensagem){
-        return new FacesMessage(level, mensagem, null);
+    private static void message(FacesMessage.Severity level, String message){
+        FacesProducer.getFacesContext().addMessage(null, getMessage(level, message));
+    }
+
+    private static FacesMessage getMessage(FacesMessage.Severity level, String message){
+        return new FacesMessage(level, message, null);
     }
 }
