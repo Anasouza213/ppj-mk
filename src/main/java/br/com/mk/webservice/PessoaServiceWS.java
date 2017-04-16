e8eaceac8c99bd5d1c7824024eecc742d10bed79
mcpedrosa <mcpedrosa@stefanini.com>
2017-04-16 10:40:02 -0300

    Add module webservice.

---
+++ src/main/java/br/com/mk/webservice/PessoaServiceWS.java
@@ -0,0 +1,50 @@
+package br.com.mk.webservice;
+
+import br.com.mk.entity.Pessoa;
+import br.com.mk.service.PessoaService;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+
+@Path("/api-pessoa")
+@RequestScoped
+public class PessoaServiceWS {
+
+    @Inject
+    private PessoaService pessoaService;
+
+    //@GET
+    //@Produces(MediaType.APPLICATION_JSON)
+    //public Collection<Pessoa> findAll(){
+    //  return pessoaService.findAll();
+    //}
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Pessoa save(Pessoa pessoa){
+        return pessoaService.save(pessoa);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Pessoa find(@PathParam("id") Long id){
+        return pessoaService.find(id);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void delete(@PathParam("id") Long id){
+        pessoaService.delete(id);
+    }
+
+
+}
---
+++ src/main/java/br/com/mk/webservice/config/ApplicationWS.java
@@ -0,0 +1,8 @@
+package br.com.mk.webservice.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("service")
+public class ApplicationWS extends Application{
+}
