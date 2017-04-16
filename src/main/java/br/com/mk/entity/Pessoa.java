a8c39e69842c1b6d8494a1c2c28a77241d432186
mcpedrosa <mcpedrosa@stefanini.com>
2017-04-16 10:48:36 -0300

    Add methods getter to id.

--- src/main/java/br/com/mk/entity/Pessoa.java
+++ src/main/java/br/com/mk/entity/Pessoa.java
@@ -21,6 +21,10 @@ public class Pessoa implements Serializable{
         this.nome = nome;
     }
 
+    public Long getId(){
+        return this.id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
