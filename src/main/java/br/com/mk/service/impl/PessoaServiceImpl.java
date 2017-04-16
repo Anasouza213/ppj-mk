64e673ad7485ada062025643fd0f8623405ea96d
mcpedrosa <mcpedrosa@stefanini.com>
2017-04-16 10:48:11 -0300

    Add methods.

--- src/main/java/br/com/mk/service/impl/PessoaServiceImpl.java
+++ src/main/java/br/com/mk/service/impl/PessoaServiceImpl.java
@@ -8,6 +8,7 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
+import java.util.Collection;
 
 @Stateless
 public class PessoaServiceImpl implements PessoaService{
@@ -19,5 +20,41 @@ public class PessoaServiceImpl implements PessoaService{
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Pessoa save(Pessoa pessoa) {
         return pessoaRepository.save(pessoa);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Pessoa change(Pessoa pessoa) {
+        return pessoaRepository.change(pessoa);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Boolean delete(Pessoa pessoa) {
+        return pessoaRepository.delete(pessoa);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void delete(Long id) {
+        pessoaRepository.delete(id);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Pessoa find(Long id) {
+        return pessoaRepository.find(id);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Pessoa find(Pessoa pessoa) {
+        return pessoaRepository.find(pessoa);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Collection<Pessoa> findAll() {
+        return pessoaRepository.findAll();
     }
 }
